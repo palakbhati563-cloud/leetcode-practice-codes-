516e0e00bd6e5d06e0117d344e57b1ac5241334e
palakbhati563-cloud <palakbhati563@gmail.com>
2025-12-30 15:56:13 +0530

    Update move_zeroes.py

---
+++ move_zeroes.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        pos = 0
+        for num in nums:
+            if num != 0:
+                nums[pos] = num
+                pos +=1
+        while pos < len(nums):
+                nums[pos]=0
+                pos+= 1        
